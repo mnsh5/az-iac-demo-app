777de7180ba43ca444dbce6df0a020c1c994210b
mnsh4 <hugo-orlando.gonzalez@atos.net>
2024-02-02 17:31:47 -0300

    :tada: add new features

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@
 #  Main Resource Group of APP        #
 ######################################
 module "az_resource_group" {
-  source     = "./modules/Resource_Group"
+  source     = "./modules/az-rg"
+  rg_name    = var.rg_name
   m_location = var.m_location
 }
--- modules/az-rg/variables.tf
+++ modules/az-rg/variables.tf
@@ -1,5 +1,9 @@
+variable "rg_name" {
+  type        = string
+  description = "Name of the resource group"
+}
+
 variable "m_location" {
   type        = string
   description = "Location where resources should be deployed"
-  default     = "East US"
 }
